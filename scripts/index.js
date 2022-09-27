689e9b92f0699031f99053e8b7d6f8f9a1f2ef81
Pavlo Cherevach <pf4pad.fr@gmail.com>
2022-09-27 11:40:39 +0300

    day1_3

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+import Swiper from '../lib/swiper-bundle.esm.browser.min.js'
+new Swiper('.goods__block', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    }
+
+  },
+  navigation: {
+    prevEl: '.goods__arrow_prev',
+    nextEl: '.goods__arrow_next',
+  }
+})
